20fd520af4305929dca5edc91f9c5d3ad91836e8
Basil Saji <62460730+basil-b2s@users.noreply.github.com>
2024-02-14 00:01:39 +0530

    CustomSort

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using CustomSort;
+//using Sort;
+
+class Program
+{
+    public List<Student> students;
+    static void Main(string[] args)
+    {
+
+        List<Student> students = new List<Student>
+        {
+            new Student { Id = 100, Name = "Ram", Age = 15, Score = 99 },
+            new Student { Id = 121, Name = "Arjun", Age = 19, Score = 89.8 },
+            new Student { Id = 101, Name = "Rahul", Age = 15, Score = 99.9 },
+            new Student { Id = 102, Name = "Riya", Age = 16, Score = 78.5 }
+        };
+
+
+        Sort.BubbleSort(students, (x, y) => x.Name.CompareTo(y.Name));
+
+        Console.WriteLine("Sorted based on name");
+        PrintData(students);
+
+        Sort.BubbleSort(students, (x, y) => x.Age.CompareTo(y.Age));
+
+        Console.WriteLine("Sorted based on Age");
+        
+        PrintData(students);
+
+        Sort.BubbleSort(students, (x, y) => Comparer<double>.Default.Compare(x.Score, y.Score));
+
+        Console.WriteLine("Sorted based on Score");
+
+        PrintData(students);
+    }
+
+    static void PrintData(List<Student> students)
+    {
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Age: {student.Age}, Score: {student.Score}");
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSort
+{
+    public static class Sort
+    {
+        public static void BubbleSort<T>(List<T> dataList, Comparison<T> comparison) 
+        { 
+            if(dataList == null || comparison == null)
+            {
+                throw new ArgumentNullException();
+            }
+            int n = dataList.Count;
+            for (int i = 0; i< n-1; i++)
+            {
+                for (int j = 0; j< n-i-1; j++)
+                {
+                    if (comparison(dataList[j], dataList[j+1]) > 0)
+                    {
+                        T temp = dataList[j];
+                        dataList[j] = dataList[j+1];
+                        dataList[j+1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
